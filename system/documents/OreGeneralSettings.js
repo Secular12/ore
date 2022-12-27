ee3bef0387c5f715237f1ef9bd6cb1ff7557cd2d
Mark <mark.dekin@gmail.com>
2022-12-26 22:02:17 -0500

    feat(settings): Add initial diceSize settings
    added under the mechanicSettings umbrella to possibly help define
    differing concerns

--- system/documents/OreGeneralSettings.js
+++ system/documents/OreGeneralSettings.js
@@ -24,15 +24,58 @@ export default class OreGeneralSettings extends FormApplication {
     }
 
     getData() {
-        return {}
+        const mechanicSettings = this.deserializeMechanicSettings()
+        
+        const data = {
+            diceSizeChoices: {10: '10', 6: '6'},
+            mechanicSettings,
+        }
+
+        Logger()('OreGeneralSettings.getData data', data)
+        
+        return data 
     }
 
     async _updateObject(event, formData) {
+        Logger()(
+            'OreGeneralSettings._updateObject event, formData',
+            event,
+            formData,
+            'OreGeneralSettings._updateObject event.target.name',
+            event.target.name
+        )
+
+        if (['diceSize'].includes(event.target.name)) {
+            const mechanicSettings = this.serializeMechanicSettings(formData)
+
+            await game.settings.set('ore', 'mechanicSettings', mechanicSettings)
+
+            this.render(true)
+        }
     }
 
     activateListeners(html) {
         super.activateListeners(html)
 
         Logger()('OreGeneralSettings.activateListeners html:', html)
+    }
+
+    deserializeMechanicSettings () {
+        const mechanicSettings = game.settings.get('ore', 'mechanicSettings')
+
+        return {
+            ...mechanicSettings,
+            diceSize: mechanicSettings.diceSize.toString()
+        }
+    }
+
+    serializeMechanicSettings (data) {
+        const expandedData = expandObject(data)
+        const mechanicSettings = game.settings.get('ore', 'mechanicSettings')
+
+        return {
+            ...mechanicSettings,
+            diceSize: +expandedData.diceSize
+        }
     }
 }

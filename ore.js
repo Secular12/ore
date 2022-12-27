7ee97ca355f68883965a84c52bfa48dfd2c4a20b
Mark <mark.dekin@gmail.com>
2022-12-26 22:01:47 -0500

    feat: register settings

--- ore.js
+++ ore.js
@@ -1,7 +1,10 @@
 import Logger from './lib/Logger.js'
+import { registerSettings } from './system/settings.js'
 
 Hooks.once('init', () => {
     CONFIG.debug.logs = true
 
     Logger()('Initializing ORE system...')
+
+    registerSettings()
 })
